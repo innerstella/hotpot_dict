1fc19203be9d0af0bff771bb962583f8a1447723
innerstella <suji0509@naver.com>
2026-02-21 17:58:16 +0900

    add: redirect

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,12 @@ import styled from "styled-components";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    alert("더이상 사용되지 않는 페이지입니다. 트위터 맛집 검색기 내에서 이용해주세요 :)");
+    window.location.href = "https://twitter-michelin.web.app/hotpot";
+  }, []);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
